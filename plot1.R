97f5f9cf77cbc0d9274f84ecd9ed9db894a2097a
karategirl20 <49295820+karategirl20@users.noreply.github.com>
2019-06-18 15:33:11 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 data <- read.table("C:/Users/sroy/Documents/R/Exploratory Data Analysis/household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
-# Get the dimensions so that it matches the dataset as prescribed by the instructions
+# Get the dimensions so that it matches the dataset as prescribed by the instructions provided in the assignment sheet
 dim(data)
 # consists of 9 columns and 2075259 rows
 #Will use data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from
